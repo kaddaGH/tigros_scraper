eb532e6f1d961cdd28b66168747660c790844197
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-25 11:02:10 +0100

    Update scraper.

---
+++ parsers/products_search.rb
@@ -0,0 +1,57 @@
+data = JSON.parse(content)
+scrape_url_nbr_products = data['data']['page']['totItems'].to_i
+current_page = data['data']['page']['selPage'].to_i
+page_size = data['data']['page']['itemsPerPage'].to_i
+number_of_pages = data['data']['page']['totPages'].to_i
+products = data['data']['products']
+
+# if ot's first page , generate pagination
+if current_page == 1 and number_of_pages > 1
+  nbr_products_pg1 = products.length
+  step_page = 1
+  while step_page <= number_of_pages
+
+    pages << {
+        page_type: 'products_search',
+        method: 'GET',
+        url: page['url'].gsub(/page=1/, "page=#{step_page}"),
+        vars: {
+            'input_type' => page['vars']['input_type'],
+            'search_term' => page['vars']['search_term'],
+            'page' => step_page,
+            'nbr_products_pg1' => nbr_products_pg1
+        }
+    }
+
+    step_page = step_page + 1
+
+
+  end
+elsif current_page == 0 and number_of_pages == 1
+  nbr_products_pg1 = products.length
+else
+  nbr_products_pg1 = page['vars']['nbr_products_pg1']
+end
+
+
+products.each_with_index do |product, i|
+
+
+  pages << {
+      page_type: 'product_details',
+      method: 'GET',
+      url: "https://www.tigros.it/ebsn/api/products?slug=#{product['slug']}&timeslotId=0&warehouseId=0&search_term=#{page['vars']['search_term']}&page=#{current_page + 1}&rank=#{ i + 1}",
+      vars: {
+          'input_type' => page['vars']['input_type'],
+          'search_term' => page['vars']['search_term'],
+          'product_rank' => i + 1,
+          'page' => current_page ,
+          'nbr_products_pg1' => nbr_products_pg1,
+          'scrape_url_nbr_products' => scrape_url_nbr_products
+
+      }
+  }
+
+
+end
+
--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -2,7 +2,7 @@ require 'cgi'
 pages << {
     page_type: 'products_search',
     method: 'GET',
-    url: 'https://www.tigros.it/ebsn/api/products?page=0&page_size=36&parent_category_id=1270110&timeslotId=0&warehouseId=0',
+    url: 'https://www.tigros.it/ebsn/api/products?page=1&page_size=36&parent_category_id=1270110&timeslotId=0&warehouseId=0',
     vars: {
         'input_type' => 'taxonomy',
         'search_term' => '-',
@@ -14,11 +14,11 @@ pages << {
 
 search_terms = ["Red Bull", "RedBull", "Energy Drink", "Energy Drinks"]
 search_terms.each do |search_term|
-
+break
   pages << {
       page_type: 'products_search',
       method: 'GET',
-      url: "https://www.tigros.it/ebsn/api/products?page=0&page_size=24&q=#{CGI.escape(search_term)}&sort=rank&timeslotId=0&warehouseId=0",
+      url: "https://www.tigros.it/ebsn/api/products?page=1&page_size=24&q=#{CGI.escape(search_term)}&sort=rank&timeslotId=0&warehouseId=0",
       vars: {
           'input_type' => 'search',
           'search_term' => search_term,
